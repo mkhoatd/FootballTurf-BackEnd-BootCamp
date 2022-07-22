06cce13bb76da81124518e449bf5c2e037e365f4
mkhoatd <binnhatle@gmail.com>
2022-07-22 09:57:13 +0700

    update user

--- src/WebApi.Domain/Entities/User.cs
+++ src/WebApi.Domain/Entities/User.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using WebApi.Domain.Enum;
 
 namespace WebApi.Domain.Entities
 {
-    public class User: BaseEntity
+    public class User : BaseEntity
     {
-        public string Name { get; set; } = string.Empty;
+        public string Name { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public UserRole Role { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
---
+++ src/WebApi.Domain/Enum/UserRole.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Domain.Enum;
+
+public enum UserRole
+{
+    Admin,
+    ProductOwner,
+    Customer
+}
